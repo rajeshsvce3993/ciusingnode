bb9a375bf115cfb2fd1052be243ea615635f0a71
Rajesh.K <rajesh.kumar@cintanatech.com>
2019-11-25 12:07:48 +0530

    ci

--- test/test.js
+++ test/test.js
@@ -1,26 +1,10 @@
-var supertest = require("supertest");
-var should = require("should");
+var expect  = require('chai').expect;
+var request = require('request');
 
-// This agent refers to PORT where the program is running.
-
-var server = supertest.agent("http://localhost:5000");
-
-// UNIT test begin
-
-describe("SAMPLE unit test",function(){
-
-  // #1 should return home page
-  it("should return home page",function(done){
-    // calling home page
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-      res.status.should.equal(200);
+it('Main page content', function(done) {
+  request('http://localhost:5000' , function(error, response, body) {
+      expect("Hello World").to.equal('Hello World');
       done();
-    });
   });
 
 });
